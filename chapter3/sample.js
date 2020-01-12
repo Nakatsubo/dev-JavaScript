10b4c90cfe6ed539ab8adfdb30da6b5b565760a5
NakatsuboYusuke <>
2020-01-12 14:00:50 +0900

    study chapter3 section52

--- chapter3/sample.js
+++ chapter3/sample.js
@@ -50,3 +50,43 @@ userList.forEach((userData) => {
     </div>
   `;
 });
+
+// 配列の先頭/末尾に要素を追加する
+// unshift => 先頭
+const a5 = ['banana', 'starwberry'];
+a5.unshift('apple');
+console.log(a5);
+// => ["apple", "banana", "starwberry"]
+// push => 末尾
+const a6 = ['apple', 'banana', 'starwberry'];
+a6.push('orange');
+console.log(a6);
+// =>  ["apple", "banana", "starwberry", "orange"]
+
+// 配列の先頭/末尾に要素を削除する
+// shift => 先頭
+const a7 = ['apple', 'banana', 'starwberry'];
+const shiftedA7 = a7.shift();
+console.log(a7);
+console.log(shiftedA7);
+// => ["banana", "starwberry"]
+// => apple
+// pop => 末尾
+const a8 = ['apple', 'banana', 'starwberry'];
+const popedA8 = a8.pop();
+console.log(a8);
+console.log(popedA8);
+// => ["apple", "banana"]
+// => starwberry
+const a9 = [];
+const popedA9 = a9.pop();
+console.log(popedA9);
+// => undefined
+
+// 配列の要素を指定位置から取り出しつつ、追加する
+// splice
+const a10 = ['apple', 'banana', 'starwberry'];
+// インデックス番号1の位置から、1つ要素を取り出し、要素'orange'を追加する
+a10.splice(1, 1, 'orange');
+console.log(a10);
+// => ["apple", "orange", "starwberry"]
