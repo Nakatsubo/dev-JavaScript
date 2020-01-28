9365bb1a49075a9da443207a5bb6e02877cd016a
NakatsuboYusuke <>
2020-01-28 21:32:14 +0900

    study Chapter8 section155

--- chapter8/sample.js
+++ chapter8/sample.js
@@ -231,3 +231,17 @@
 // weather.outerHTML = '<img src="sample.jpg" alt="サンプル">';
 // console.log(weather);
 // // => <img src="sample.jpg" alt="サンプル">
+
+// 要素の属性を取得、書き換える
+// ノード.setAttriute(属性名, 値) => 要素の属性を設定
+const imageElement = document.querySelector('#image');
+imageElement.setAttribute('src', 'sample.png');
+console.log(imageElement);
+// => <img src="sample.png" id="image">
+
+// ノード.getAttribute(属性名) => 要素の属性を取得
+const anchorElement = document.querySelector('#anchor');
+console.log(anchorElement.getAttribute('href'));
+// => https://example.com
+
+// ノード.hasAttribute(属性名) => 要素の属性があるかどうか
