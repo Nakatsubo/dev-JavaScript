af7133c3309083e158e54284b8e4b3064846f677
NakatsuboYusuke <>
2020-01-31 22:12:02 +0900

    study Chapter9 section165

--- chapter9/sample.js
+++ chapter9/sample.js
@@ -9,3 +9,18 @@
 //   const log = document.querySelector('.log');
 //   log.innerHTML = event.target.value;
 // };
+
+// チェックボックスの情報を取得
+// 未チェック
+const checkBoxA = document.querySelector('#checkBoxA');
+console.log(checkBoxA.checked);
+// => false
+// チェック済
+const checkBoxB = document.querySelector('#checkBoxB');
+console.log(checkBoxB.checked);
+// => true
+
+// チェックされた状態に変化
+checkBoxA.checked = true;
+console.log(checkBoxA.checked);
+// => true
