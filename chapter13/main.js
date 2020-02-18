a9060bd6d39be00d4e4772ff34fad856cffb4202
NakatsuboYusuke <>
2020-02-18 15:17:36 +0900

    study Chapter13 section 219

--- chapter13/main.js
+++ chapter13/main.js
@@ -1,1 +1,8 @@
-
+// setTimeout(関数, ミリ秒) => 一定時間後に処理を行う
+console.log('起動時の時刻', new Date().toLocaleTimeString());
+setTimeout(() => {
+  console.log('setTimeout後の時刻', new Date().toLocaleString());
+}, 1000);
+// => 実行結果
+// 起動時の時刻 15:15:37
+// setTimeout後の時刻 2020/2/18 15:15:38
