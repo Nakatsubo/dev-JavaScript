95e802c63c61840b45d8dd288832209fbfba43f3
NakatsuboYusuke <>
2020-02-25 17:16:51 +0900

    study Chapter14 section230

--- chapter14/main.js
+++ chapter14/main.js
@@ -1,12 +1,19 @@
-// JSONをパースする
-const jsonString = `{ "students": 40, "grade": 4, "name": "C組" }`;
-// 文字列をJSONオブジェクトに変換
-const data = JSON.parse(jsonString);
-console.log(data);
-// => Object
-console.log(data.students);
-// => 40
-console.log(data.grade);
-// => 4
-console.log(data.name);
-// => C組
+// // JSONをパースする
+// const jsonString = `{ "students": 40, "grade": 4, "name": "C組" }`;
+// // 文字列をJSONオブジェクトに変換
+// const data = JSON.parse(jsonString);
+// console.log(data);
+// // => Object
+// console.log(data.students);
+// // => 40
+// console.log(data.grade);
+// // => 4
+// console.log(data.name);
+// // => C組
+
+// オブジェクトをJSONに変換する
+const data = { a: 1000, b: 'Hello, World!' }
+// オブジェクトをJSONに変換する
+const str = JSON.stringify(data);
+console.log(str);
+// => {"a":1000,"b":"Hello, World!"}
